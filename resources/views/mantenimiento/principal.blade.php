4b6148c8c9a91871a823b98eee5c4bb268d6a3cb
MARCOSFX <paredeschis@gmail.com>
2016-09-30 01:37:14 -0500

    desarrollo de la vista de mantenimiento

--- resources/views/mantenimiento/principal.blade.php
+++ resources/views/mantenimiento/principal.blade.php
@@ -2,6 +2,7 @@
 @section('vistainicial')
 @stop
 @section('menu_modulos')
+    {{--continuar--}}
 
         <div>
             <ul class="nav nav-tabs">
@@ -10,7 +11,55 @@
                 </li>
             </ul>
         </div>
+        <div class="text-center" style="padding-top:.1rem; margin-top: 0">
+            <h3 style="font-weight: bold">BIENVENIDOS A MANTENIMEINTO</h3>
+        </div>
+        <br>
+        <div class="container col-md-12">
 
+            <div class="row col-lg-offset-6">
+                <i class="fa fa-5x fa fa-wrench JustifyCenter"></i>
+
+            </div>
+            <hr style="height: .5px ; background-color: green; width: 50%"/>
+        </div>
+
+
+        <div class="col-lg-12" style="padding: 9px; text-align: center">
+            <div class="col-lg-3">
+                <div class="col-lg-12"><span><i style="font-size: 2.3rem" class="fa fa-cart-arrow-down" aria-hidden="true"> <span style="font-weight: bold">Productos</span></i></span></div>
+                <div class="col-lg-12" style=" padding-top: .5rem"><button style="text-align:center;   width:170px; height:45px" type="button" class="btn btn-w-m btn-success color-btn hover-1">Tipo Producto</button></div>
+                <div class="col-lg-12" style=" padding-top: .5rem"><button style="text-align:center;   width:170px; height:45px" type="button" class="btn btn-w-m btn-success color-btn hover-1">Categoria</button></div>
+                <div class="col-lg-12" style=" padding-top: .5rem"><button style="text-align:center;   width:170px; height:45px" type="button" class="btn btn-w-m btn-success color-btn hover-1">Marca</button></div>
+                <div class="col-lg-12" style=" padding-top: .5rem"><button style="text-align:center;   width:170px; height:45px" type="button" class="btn btn-w-m btn-success color-btn hover-1">Modelo</button></div>
+                <div class="col-lg-12" style=" padding-top: .5rem"><button style="text-align:center;   width:170px; height:45px" type="button" class="btn btn-w-m btn-success color-btn hover-1">Unidad Medida</button></div>
+            </div>
+
+            <div class="col-lg-3">
+                <div class="col-lg-12"><span><i style="font-size: 2.3rem" class="fa fa-users" aria-hidden="true"> <span style="font-weight: bold">Clientes</span></i></span></div>
+                <div class="col-lg-12" style=" padding-top: .5rem"><button style="text-align:center;   width:170px; height:45px" type="button" class="btn btn-w-m btn-success color-btn hover-1">Tipo Cliente</button></div>
+                <div class="col-lg-12" style=" padding-top: .5rem"><button style="text-align:center;   width:170px; height:45px" type="button" class="btn btn-w-m btn-success color-btn hover-1">Estado Civil</button></div>
+                <div class="col-lg-12" style=" padding-top: .5rem"><button style="text-align:center;   width:170px; height:45px" type="button" class="btn btn-w-m btn-success color-btn hover-1">Grado de Instruccion</button></div>
+            </div>
+            <div class="col-lg-3">
+                <div class="col-lg-12"><span><i style="font-size: 2.3rem" class="fa fa-cart-arrow-down" aria-hidden="true"> <span style="font-weight: bold">Productos</span></i></span></div>
+                <div class="col-lg-12" style=" padding-top: .5rem"><button style="text-align:center;   width:170px; height:45px" type="button" class="btn btn-w-m btn-success color-btn hover-1">Tipo.Prodct</button></div>
+                <div class="col-lg-12" style=" padding-top: .5rem"><button style="text-align:center;   width:170px; height:45px" type="button" class="btn btn-w-m btn-success color-btn hover-1">Categoria</button></div>
+                <div class="col-lg-12" style=" padding-top: .5rem"><button style="text-align:center;   width:170px; height:45px" type="button" class="btn btn-w-m btn-success color-btn hover-1">Marca</button></div>
+                <div class="col-lg-12" style=" padding-top: .5rem"><button style="text-align:center;   width:170px; height:45px" type="button" class="btn btn-w-m btn-success color-btn hover-1">Modelo</button></div>
+                <div class="col-lg-12" style=" padding-top: .5rem"><button style="text-align:center;   width:170px; height:45px" type="button" class="btn btn-w-m btn-success color-btn hover-1">Unidad.M</button></div>
+            </div>
+            <div class="col-lg-3">
+                <div class="col-lg-12"><span><i style="font-size: 2.3rem" class="fa fa-cart-arrow-down" aria-hidden="true"> <span style="font-weight: bold">Productos</span></i></span></div>
+                <div class="col-lg-12" style=" padding-top: .5rem"><button style="text-align:center;   width:170px; height:45px" type="button" class="btn btn-w-m btn-success color-btn hover-1">Categoria</button></div>
+                <div class="col-lg-12" style=" padding-top: .5rem"><button style="text-align:center;   width:170px; height:45px" type="button" class="btn btn-w-m btn-success color-btn hover-1">Tipo.Prodct</button></div>
+                <div class="col-lg-12" style=" padding-top: .5rem"><button style="text-align:center;   width:170px; height:45px" type="button" class="btn btn-w-m btn-success color-btn hover-1">Categoria</button></div>
+                <div class="col-lg-12" style=" padding-top: .5rem"><button style="text-align:center;   width:170px; height:45px" type="button" class="btn btn-w-m btn-success color-btn hover-1">Marca</button></div>
+                <div class="col-lg-12" style=" padding-top: .5rem"><button style="text-align:center;   width:170px; height:45px" type="button" class="btn btn-w-m btn-success color-btn hover-1">Unidad.M</button></div>
+            </div>
+        </div>
 
 @endsection
+
+
 
