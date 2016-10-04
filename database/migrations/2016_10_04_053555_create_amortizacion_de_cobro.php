34a95f997c78c2a888bdbab24e575c2fb9a7e5e0
CarlosMoralesX <lcarlosl_17@hotmail.com>
2016-10-04 01:16:04 -0500

    nuevas migration

---
+++ database/migrations/2016_10_04_053555_create_amortizacion_de_cobro.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAmortizacionDeCobro extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('amortizacion_de_cobro', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('cronograma_cobro_id')->unsigned();
+            $table->decimal('monto');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('amortizacion_de_cobro');
+    }
+}
---
+++ database/migrations/2016_10_04_054820_create_detalle_venta.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDetalleVenta extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detalle_venta', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('venta_id')->unsigned();
+            $table->integer('producto_id')->unsigned();
+            $table->decimal('cantidad');
+            $table->decimal('descuento');
+            $table->decimal('igv');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('detalle_venta');
+    }
+}
