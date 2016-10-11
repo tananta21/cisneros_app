ff6e737b9c569af842a15de8a62d2d08e70d2363
tananta21 <terre.21az@gmail.com>
2016-10-11 02:29:09 -0500

    retornando a master

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -374,18 +374,12 @@ desired effect
     </script>
 
 
-
-
-
-
-
-
     <!-- Control Sidebar -->
     <aside class="control-sidebar control-sidebar-dark" >
 
     </aside>
 
-    <!-- /.control-sidebar -->
+
     <!-- Add the sidebar's background. This div must be placed
          immediately after the control sidebar -->
     <div class="control-sidebar-bg"></div>
