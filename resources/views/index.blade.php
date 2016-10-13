cc9bd038b0990e962f9142caf5b99c339f9cf384
tananta21 <terre.21az@gmail.com>
2016-10-12 21:36:59 -0500

    actualizacion de vistas de registrar de cliente rama roberto

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>CISNEROS SAC</title>
-    <!-- Tell the browser to be responsive to screen width -->
+
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.6 -->
     <link rel="stylesheet" href="{{url('/')}}/cisnero/css/nuestro.css">
